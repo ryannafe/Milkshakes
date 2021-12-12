f19170a9fa28cf93b56dd9f5ef71efbca963aa7d
rbryanben <rbryanben@gmail.com>
2021-12-12 15:53:15 +0200

    init

---
+++ Kalahari/Controllers/WebApp.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kalahari.Controllers
+{
+    public class WebApp : Controller
+    {
+        public IActionResult Application()
+        {
+            return View();
+        }
+    }
+
+}
---
+++ Kalahari/bin/Debug/netcoreapp3.1/publish/wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿//Application
+const app = new Vue({
+    el: "#app",
+    data: {
+        views: {
+            one: "Ever heard of Metal as a Service",
+            two: "Ever heard of ASP.NET Core",
+            three: "They all have nothing to do with flutter"
+        }
+    }
+})
